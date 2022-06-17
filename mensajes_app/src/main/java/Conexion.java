031bad9d0be19b55a138dd1d79d4060cc792fcc6
unknown <jdchuquispuma@gmail.com>
2022-06-17 09:54:58 -0500

    capa inicial

--- mensajes_app/src/main/java/Conexion.java
+++ mensajes_app/src/main/java/Conexion.java
@@ -8,9 +8,6 @@ public class Conexion {
         Connection connection = null;
         try{
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mensajes_app","root","123456");
-            if(connection != null){
-                System.out.println("conexión exitosa");
-            }
         }catch (SQLException e){
             System.out.println(e);
         }
