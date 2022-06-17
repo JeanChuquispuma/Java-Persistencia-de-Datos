c78a1aac8791dc92a7ceb56760522c332a8f90f6
unknown <jdchuquispuma@gmail.com>
2022-06-17 10:13:57 -0500

    Crud: insertar

--- mensajes_app/src/main/java/MensajesDAO.java
+++ mensajes_app/src/main/java/MensajesDAO.java
@@ -1,6 +1,27 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class MensajesDAO {
 
     public static void crearMensajeDB(Mensajes mensaje){
+        Conexion db_connect = new Conexion();
+
+        try(Connection conexion = db_connect.get_connection()){
+            PreparedStatement ps = null;
+            try{
+               String query="INSERT INTO `mensajes` (`mensaje`,`autor_mensaje`) VALUES (?,?)";
+               ps=conexion.prepareStatement(query);
+               ps.setString(1,mensaje.getMensaje());
+               ps.setString(2,mensaje.getAutor_mensaje());
+               ps.executeUpdate();
+               System.out.println("Mensaje Creado!");
+            }catch (Exception e){
+                System.out.println("Error al crear el mensaje");
+                System.out.println(e);
+            }
+        }catch (Exception e){
+            System.out.println(e);
+        }
 
     }
 
